204d571d76c99f9b757f843065cb42e296dc01ff
Tai <test>
2020-05-15 14:16:31 +0800

    springcloud(十一)：服务网关Zuul高级篇 (finish) http://www.ityouknow.com/springcloud/2018/01/20/spring-cloud-zuul.html

--- src/main/java/com/example/demo/DemoZuulApplication.java
+++ src/main/java/com/example/demo/DemoZuulApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.filter.TokenFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,5 +14,9 @@ public class DemoZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoZuulApplication.class, args);
 	}
-
+	
+	@Bean
+	public TokenFilter tokenFilter() {
+		return new TokenFilter();
+	}
 }
---
+++ src/main/java/com/example/demo/filter/TokenFilter.java
@@ -0,0 +1,58 @@
+package com.example.demo.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class TokenFilter extends ZuulFilter {
+	
+	private final Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+	
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+	
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        logger.info("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString());
+
+        String token = request.getParameter("token");// 获取请求的参数
+
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+	}
+
+}
